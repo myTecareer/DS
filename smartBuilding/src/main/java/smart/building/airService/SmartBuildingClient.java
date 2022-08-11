e494621b91e687a0f0860029cc34e76a815d469c
myTecareer <allison.xu723@gmail.com>
2022-08-11 20:42:02 +0100

    service1 client comments

--- smartBuilding/src/main/java/smart/building/airService/SmartBuildingClient.java
+++ smartBuilding/src/main/java/smart/building/airService/SmartBuildingClient.java
@@ -1,5 +1,6 @@
+// Name of the package where all the generated files are present.
 package smart.building.airService;
-
+//required java packages for the program
 import java.util.Random;
 
 import io.grpc.ManagedChannel;
@@ -7,31 +8,37 @@ import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
 import smart.building.airService.AirServiceGrpc.AirServiceBlockingStub;
 import smart.building.airService.AirServiceGrpc.AirServiceStub;
-
+//Client need not to extend any other class (GRPC related code) here
 public class SmartBuildingClient {
+	// Creating stubs for establishing the connection with server.
+	// Blocking stub
 	private static AirServiceBlockingStub blockingStub;
+	// asyncStub
 	private static AirServiceStub asyncStub;
 	public static void main(String[] args) throws Exception{
-		
-		ManagedChannel channel = ManagedChannelBuilder
+		// First a channel is being created to the server from client. Here, we provide the server name (localhost) and port (50058).
+
+	ManagedChannel channel = ManagedChannelBuilder
 				.forAddress("localhost", 50072)
 				.usePlaintext()
 				.build();
-		
+		//these stubs are generated from proto
 		blockingStub = AirServiceGrpc.newBlockingStub(channel);
 		asyncStub = AirServiceGrpc.newStub(channel);
-		
+		//use asyncStub
+		//Client Streaming
 		System.out.println("currAirQuality method START===");
 		currAirQuality();
-		
+		//Bi-Directional Streaming
 		System.out.println("openCloseAirPurifier method START===");
 		openCloseAirPurifier();
-		
+		// Closing the channel once message has been passed.
 		channel.shutdown();
 		
 	}
 	public static void openCloseAirPurifier() {
 		// TODO Auto-generated method stub
+		// Handling the server stream for client using onNext (logic for handling each message in stream), onError, onCompleted (logic will be executed after the completion of stream)
 		StreamObserver<AirPuriReply> responseObserver = 
 				new StreamObserver<AirPuriReply>() {
 
@@ -44,7 +51,7 @@ public class SmartBuildingClient {
 					@Override
 					public void onError(Throwable t) {
 						// TODO Auto-generated method stub
-						
+						t.printStackTrace();
 					}
 
 					@Override
@@ -55,15 +62,14 @@ public class SmartBuildingClient {
 				
 			
 				};
-				
+				// Here, we are calling the Remote openCloseAirPurifier method. Using onNext, client sends a stream of messages.
 				StreamObserver<AirPuriRequest> requestObserver = asyncStub.openCloseAirPurifier(responseObserver);
 				try {
 					requestObserver.onNext(AirPuriRequest.newBuilder().setVal("pm2.5").setFigure(150).build());
 					requestObserver.onNext(AirPuriRequest.newBuilder().setVal("pm2.5").setFigure(200).build());
-					//requestObserver.onNext(AirPuriRequest.newBuilder().
 					
 					System.out.println("sending message");
-					
+					// Mark the end of requests
 					requestObserver.onCompleted();
 					//sleep for a while before sending the next one
 					Thread.sleep(new Random().nextInt(2000) + 500);
@@ -76,13 +82,12 @@ public class SmartBuildingClient {
 	}
 	public static void currAirQuality() {
 		// TODO Auto-generated method stub
+		// Handling the stream for client using onNext (logic for handling each message in stream), onError, onCompleted (logic will be executed after the completion of stream)
 		StreamObserver<CurrentAirReply> responseObserver = new StreamObserver<CurrentAirReply>() {
 			
 			@Override
 			public void onNext(CurrentAirReply value) {
 				// TODO Auto-generated method stub
-				//System.out.println("currAirQuality method START===client3");
-				//System.out.println("receiving item " + value.getMessage());
 				System.out.println("currAirQuality method STARTclient3 " + value.getMessage());
 			}
 
@@ -95,25 +100,23 @@ public class SmartBuildingClient {
 			@Override
 			public void onCompleted() {
 				// TODO Auto-generated method stub
-				//System.out.println("currAirQuality method START===client4");
 				System.out.println("stream is completed...receiving currAirQuality method.");
 				
 			}
 			
 		};
-		
-		StreamObserver<CurrentAirRequest> requestObserve = asyncStub.currAirQuality(responseObserver);
+		// Here, we are calling the Remote currAirQuality method. Using onNext, client sends a stream of messages.
+			StreamObserver<CurrentAirRequest> requestObserve = asyncStub.currAirQuality(responseObserver);
 		
 		try {
 			requestObserve.onNext(CurrentAirRequest.newBuilder().setItem("PM2.5").build());
 			requestObserve.onNext(CurrentAirRequest.newBuilder().setItem("CO").build());
 			requestObserve.onNext(CurrentAirRequest.newBuilder().setItem("CO2").build());
 			
-			//System.out.println("currAirQuality method START===client2");
 			System.out.println("sending items.");
 			//successfully stream completion
 			requestObserve.onCompleted();
-			
+			// Sleep for a bit before sending the next one.
 			Thread.sleep(new Random().nextInt(1000) + 500);
 			
 			
