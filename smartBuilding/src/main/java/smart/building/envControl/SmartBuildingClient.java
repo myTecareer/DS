df72c54e5dc86580ddfa6db619b7f02cef64b9b9
myTecareer <allison.xu723@gmail.com>
2022-08-11 20:52:47 +0100

    update service2 server and client

--- smartBuilding/src/main/java/smart/building/envControl/SmartBuildingClient.java
+++ smartBuilding/src/main/java/smart/building/envControl/SmartBuildingClient.java
@@ -1,48 +1,58 @@
+// Name of the package where all the generated files are present.
 package smart.building.envControl;
-
+//required java packages for the program
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
-//import smart.building.ChangeReply;
-//import smart.building.ChangeRequest;
 import smart.building.envControl.BuildingEnvControlServiceGrpc.BuildingEnvControlServiceBlockingStub;
 import smart.building.envControl.BuildingEnvControlServiceGrpc.BuildingEnvControlServiceStub;
 
 public class SmartBuildingClient {
+	// Creating stubs for establishing the connection with server.
+	// Blocking stub
 	private static BuildingEnvControlServiceBlockingStub blockingStub;
+	// asyncStub
 	private static BuildingEnvControlServiceStub asyncStub;
 	
 	public static void main(String[] args) throws Exception{
-		
-		ManagedChannel channel = ManagedChannelBuilder
+		// First a channel is being created to the server from client. Here, we provide the server name (localhost) and port (50058).
+	ManagedChannel channel = ManagedChannelBuilder
 				.forAddress("localhost", 50071)
 				.usePlaintext()
 				.build();
-		
+	//these stubs are generated from proto	
 		blockingStub = BuildingEnvControlServiceGrpc.newBlockingStub(channel);
 		asyncStub = BuildingEnvControlServiceGrpc.newStub(channel);
-		
+		//use blockingStub
+		//unary
 		System.out.println("air conditionar method start====");
 		airConditionerChange();
-		
+		//use blockingStub
+		//unary
 		System.out.println("humidity change method start====");
 		humidityChange();
-		
+		// Closing the channel once message has been passed.
 		channel.shutdown();
 	}
 
 	public static void humidityChange() {
 		// TODO Auto-generated method stub
+		// First creating a request message. Here, the message contains a string and a figure in setStatus
 		HumidityRequest request = HumidityRequest.newBuilder().setStatus("increased").setFigure(10).build();
+	//  Calling a remote RPC method using blocking stub defined in main method. request is the message we want to pass.
 		HumidityReply reply = blockingStub.humidityCheck(request);
-		System.out.println("reply: "+ reply.getMessage());
+		//response contains the output from the server side. Here, we are printing the value contained by response. 
+			System.out.println("reply: "+ reply.getMessage());
 	
 	}
 
 	private static void airConditionerChange() {
 		// TODO Auto-generated method stub
 		String status = "on";
+		// First creating a request message. Here, the message contains a string  in setStatus
 		ChangeRequest request = ChangeRequest.newBuilder().setStatus(status).build();
+	//  Calling a remote RPC method using blocking stub defined in main method. request is the message we want to pass.
 		ChangeReply reply = blockingStub.airConditionerChange(request);
+		//response contains the output from the server side. Here, we are printing the value contained by response. 
 		System.out.println("reply: "+ reply.getMessage() );
 	}
 	
--- smartBuilding/src/main/java/smart/building/envControl/SmartBuildingServer.java
+++ smartBuilding/src/main/java/smart/building/envControl/SmartBuildingServer.java
@@ -1,5 +1,6 @@
+//all airService file are generated here.
 package smart.building.envControl;
-
+//required java packages for the program
 import java.io.IOException;
 import java.net.InetAddress;
 
@@ -9,22 +10,24 @@ import javax.jmdns.ServiceInfo;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
-//import smart.building.ChangeReply;
-//import smart.building.ChangeRequest;
-import smart.building.envControl.BuildingEnvControlServiceGrpc.BuildingEnvControlServiceImplBase;
 
+import smart.building.envControl.BuildingEnvControlServiceGrpc.BuildingEnvControlServiceImplBase;
+//Extend the ImplBase imported class here. It is an Interface file with required rpc methods
 public class SmartBuildingServer extends  BuildingEnvControlServiceImplBase{
 
 	public static void main(String[] args) {
+		// The smartBuildingServer is the current file name/ class name. Using an instance of this class different methods could be invoked by the client.
 		SmartBuildingServer smartBuildingServer = new SmartBuildingServer();
-		
+		//register the service
 		smartBuildingServer.registerService();
-		int port = 50071;
+		//this port number where server will be listen to
+			int port = 50071;
 		
 		try {
+			//create a server
 			Server server = ServerBuilder.forPort(port)
-					.addService(smartBuildingServer)
-					.build()
+					.addService(smartBuildingServer)//this smartBuildingServer service
+					.build()//build
 					.start();
 			System.out.println("SmartBuilding Server started, listening on " + port);
 			server.awaitTermination();
@@ -47,8 +50,11 @@ public class SmartBuildingServer extends  BuildingEnvControlServiceImplBase{
 			System.out.println(e.getMessage());
 		}
 	}
+	//rpc method //unary
+	
 	public void humidityCheck(HumidityRequest request, 
 			StreamObserver<HumidityReply> responseObserver) {
+		// Retrieve the value from the request of the client
 		System.out.println("receining Humidity check request: " + request.getStatus());
 		String msg = "";
 		if(request.getStatus().equalsIgnoreCase("increased")) {
@@ -58,11 +64,14 @@ public class SmartBuildingServer extends  BuildingEnvControlServiceImplBase{
 			msg = "Air humidity has been "+request.getStatus()+" by "+request.getFigure()+"%";
 			
 		}
+		// Preparing the reply for the client. Here, response is build and with the value (msg) computed by above logic.  
 		HumidityReply reply = HumidityReply.newBuilder().setMessage(msg).build();
-		  responseObserver.onNext(reply);
+		// Sending the reply for each request.
+	responseObserver.onNext(reply);
 
 	      responseObserver.onCompleted();
 	}
+	//rpc method //unary
 	public void airConditionerChange(ChangeRequest request,
 			StreamObserver<ChangeReply> responseObserver ) {
 		System.out.println("receiving Air conditioner change request: Turn "+request.getStatus());
@@ -73,8 +82,10 @@ public class SmartBuildingServer extends  BuildingEnvControlServiceImplBase{
 		if(request.getStatus().equalsIgnoreCase("off")) {
 			msg = "Air conditioner already turn off.";
 		}
+		// Preparing the reply for the client. Here, response is build and with the value (msg) computed by above logic.  
 		ChangeReply reply = ChangeReply.newBuilder().setMessage(msg).build();
-		  responseObserver.onNext(reply);
+		// Sending the reply for each request. 
+		responseObserver.onNext(reply);
 
 	      responseObserver.onCompleted();
 	}
